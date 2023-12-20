d1a2f36ed3da2a63afabac03b41849a08f51189a
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-12-20 15:04:35 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "22917000"))
     API_HASH = os.environ.get("API_HASH", "11b81549c2e4c3543806193f590ec0ad")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6884162735:AAFQagYaMGcaUpHsZvaUeJ4h02A86FIKGq8")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "hari")
+    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002023567286"))
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://paid:paid@cluster0.xgucvek.mongodb.net/?retryWrites=true&w=majority")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1426588906").split(" ")]
